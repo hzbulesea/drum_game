ad4133f1df79ff8681163f5ef1d1af8c09c6bc4b
crystalxshi <crystalxinceshi@gmail.com>
2019-04-26 02:16:33 -0700

    first app

---
+++ Drum_Game/app/src/main/java/drumgame/example/drum_game/PlaygroundAvtivity.java
@@ -0,0 +1,13 @@
+package drumgame.example.drum_game;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlaygroundAvtivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playground_avtivity);
+    }
+}
